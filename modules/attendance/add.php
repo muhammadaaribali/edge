41f587a1f2110ae6ebea07684b8303a13ac98bd8
muhammadaaribali <muhammadaarib7452@gmail.com>
2026-04-28 00:46:13 +0500

    attendance module

---
+++ modules/attendance/add.php
@@ -0,0 +1,31 @@
+<?php
+include("../../config/db.php");
+
+$employee=$conn->query("SELECT employee_id, name from Employee");
+?>
+
+<h2>Mark Attencance</h2>
+
+<form action="../..actions/attendance_action.php" method="POST">
+    <select name="employee_id" required>
+        <option value="">Select Employee</option>
+        <?php while($e=$employee->fetch_assoc()){ ?>
+        <option value="<?php echo $e['employee_id']; ?>">
+            <?php echo $e['name']; ?>
+        </option>
+       <?php } ?>
+    </select><br><br>
+
+    <input type="date" name="date" required><br><br>
+    <input type="time" name="check_in"><br><br>
+    <input type="time" name="check_out"><br><br>
+
+    <select name="status">
+        <option value="Present">Present</option>
+        <option value="Absent">Absent</option>
+    </select><br><br>
+
+    <button type="submit" name="add_attendance">
+        Submit
+    </button>
+</form>
---
+++ modules/attendance/view.php
@@ -0,0 +1,34 @@
+<?php
+include("../../config/db.php");
+
+$result = $conn->query("
+    SELECT a.attendance_id, e.name, a.date, a.check_in, a.check_out, a.status
+    FROM Attendance a
+    JOIN Employee e ON a.employee_id = e.employee_id
+");
+?>
+
+<h2>Attendance Records</h2>
+
+<table border="1">
+<tr>
+    <th>ID</th>
+    <th>Employee</th>
+    <th>Date</th>
+    <th>Check In</th>
+    <th>Check Out</th>
+    <th>Status</th>
+</tr>
+
+<?php while($row = $result->fetch_assoc()) { ?>
+<tr>
+    <td><?php echo $row['attendance_id']; ?></td>
+    <td><?php echo $row['name']; ?></td>
+    <td><?php echo $row['date']; ?></td>
+    <td><?php echo $row['check_in']; ?></td>
+    <td><?php echo $row['check_out']; ?></td>
+    <td><?php echo $row['status']; ?></td>
+</tr>
+<?php } ?>
+
+</table>
